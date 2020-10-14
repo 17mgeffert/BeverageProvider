6516c370c36ac22cd10f50ff8244a6c60b9e73d8
mpalu <mpalumbo20@gmail.com>
2020-10-13 20:49:05 -0400

    added another holder endpoint that will act as the official post endpoint

--- src/main/restservice/Controller.java
+++ src/main/restservice/Controller.java
@@ -7,9 +7,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import restservice.dataStorage.DataStorage;
 import restservice.dateAccess.GatherDrinkData;
 import restservice.model.Greeting;
@@ -41,12 +39,6 @@ public class Controller {
 	//endpoint to get a random recipe
 	@GetMapping("/recipe")
 	public String getRecipe() {
-		ArrayList<Ingredient> ingredients = new ArrayList<>();
-		Ingredient ingredient = new Ingredient();
-		ingredient.setIngredientId("idHold");
-		ingredient.setIngredientName("Vodka");
-		ingredients.add(ingredient);
-
 		//return ingredients;
 		return drinkData.getTestData();
 	}
@@ -65,14 +57,23 @@ public class Controller {
 
 	/**
 	 * This function is used by the frontend to gather all the ingredients
-	 * that need to be dispalyed on the frontend
+	 * that need to be displayed on the frontend
 	 * @return
 	 */
 	@GetMapping("/ingredients-list")
 	public String getDisplayIngredients(){
-		//Ingredient[] i = drinkData.getAllIngredients();
-		String json = new Gson().toJson(drinkData.getAllIngredients());
-		return json;
+		return new Gson().toJson(drinkData.getAllIngredients());
+	}
+
+	/**
+	 * The post call that will occur when the user selected x number of ingredients
+	 * and needs a random recipe returned
+	 * @param submittedIngredients
+	 * @return
+	 */
+	@PostMapping("/find-recipe")
+	public String postRandomRecipe(@RequestBody Ingredient[] submittedIngredients){
+		return "test";
 	}
 
 }
