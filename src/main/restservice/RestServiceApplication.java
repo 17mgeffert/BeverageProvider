d511d585d173b94278b6433cf3a935cf4716be58
mpalu <mpalumbo20@gmail.com>
2020-10-26 15:03:51 -0400

    trying to add better error handling

--- src/main/restservice/RestServiceApplication.java
+++ src/main/restservice/RestServiceApplication.java
@@ -3,6 +3,7 @@ package restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import restservice.dateAccess.GatherDrinkData;
 
 @SpringBootApplication
@@ -11,6 +12,8 @@ public class RestServiceApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(RestServiceApplication.class, args);
         GatherDrinkData data = context.getBean(GatherDrinkData.class);
+        DispatcherServlet dispatcherServlet = (DispatcherServlet)context.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
         data.initialize();
 
     }
