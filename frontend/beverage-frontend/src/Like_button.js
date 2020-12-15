fe2af7a773f8d8719033d0e8b76853a8033ff745
jmelrose8094 <melrosj@sunypoly.edu>
2020-12-15 16:00:37 -0500

    edited js file

--- frontend/beverage-frontend/src/Like_button.js
+++ frontend/beverage-frontend/src/Like_button.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 const e = React.createElement;
 
@@ -18,6 +18,63 @@ class LikeButton extends React.Component {
       { onClick: () => this.setState({ liked: true }) },
       'Like'
     );
+  }
+}
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);*/
+'use strict';
+
+const e = React.createElement;
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+
+  componentDidMount() {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 const domContainer = document.querySelector('#like_button_container');
